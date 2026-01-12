967e7091cd02e64aeace63546ba641e2b3859241
Vk <s10517218@gmail.com>
2026-01-12 11:19:03 +0530

    Add files via upload

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -1,8 +1,12 @@
 import math
 
 from pyrogram.types import InlineKeyboardButton
-
+import config
+from AnonXMusic import app
 from AnonXMusic.utils.formatters import time_to_seconds
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
@@ -31,55 +35,49 @@ def stream_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
-    umm = math.floor(percentage)
-    if 0 < umm <= 10:
-        bar = "⚪─────────"
-    elif 10 < umm < 20:
-        bar = "━⚪────────"
-    elif 20 <= umm < 30:
-        bar = "━━⚪───────"
-    elif 30 <= umm < 40:
-        bar = "━━━⚪──────"
-    elif 40 <= umm < 50:
-        bar = "━━━━⚪─────"
-    elif 50 <= umm < 60:
-        bar = "━━━━━⚪────"
-    elif 60 <= umm < 70:
-        bar = "━━━━━━⚪───"
-    elif 70 <= umm < 80:
-        bar = "━━━━━━━⚪──"
-    elif 80 <= umm < 95:
-        bar = "━━━━━━━━⚪─"
-    else:
-        bar = "━━━━━━━━━⚪"
+    anon = math.floor(percentage)
+
+    progress_bars = [
+        "⚪─────────", "━⚪────────", "━━⚪───────", "━━━⚪──────", "━━━━⚪─────",
+        "━━━━━⚪────", "━━━━━━⚪───", "━━━━━━━⚪──", "━━━━━━━━⚪─", "━━━━━━━━━⚪﻿"
+    ]
+
+    ba = progress_bars[min(anon // 10, 9)]
+##bar of wynk---------------------------------------
+
+
+
     buttons = [
         [
             InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
+                text=f"{played} {ba} {dur}",
                 callback_data="GetTimer",
             )
         ],
-        [   
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        [
+            InlineKeyboardButton(text="◁ 20s", callback_data=f"seekback {chat_id} 20"),
+            #InlineKeyboardButton(text="10s", callback_data=f"seekback {chat_id} 10"),
+            InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
+            #InlineKeyboardButton(text="10s", callback_data=f"seek {chat_id} 10"),
+            InlineKeyboardButton(text="20s ▷", callback_data=f"seek {chat_id} 20"),
         ],
+        [
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ",url=f"https://t.me/{app.username}?startgroup=true",
+            ),
+        ],
+
+        # [
+        #     InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"), 
+        # ],
     ]
-    
     return buttons
+
+
 
 
 def stream_markup(_, chat_id):
     buttons = [
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -89,11 +87,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"AviaxPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"AviaxPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
@@ -131,7 +129,8 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
             InlineKeyboardButton(
                 text=_["P_B_1"],
                 callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
-            ),
+
+),
             InlineKeyboardButton(
                 text=_["P_B_2"],
                 callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
